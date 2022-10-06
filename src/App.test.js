47b4634edd692833fff1827b960b41ddb54c2d0c
harsh-mayank <harsh.mayank40@gmail.com>
2022-10-06 19:20:34 +0530

    Tests for 1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders 1", () => {
+  render(<App/>)
+  const fizzbuzz = screen.getByText('1');
+  expect(fizzbuzz).toBeInTheDocument(1);
 });
